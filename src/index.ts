0854adaa39dab0e719e00d4b8e1d2c79c87aa4a3
jejolare <jejolare@gmail.com>
2024-11-27 23:37:36 +0700

    update exports

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
-export * from './components/Alert';
-export * from './components/Footer';
-export * from './components/Modal';
-export * from './components/Popup';
-export * from './components/Preloader';
-export * from './utils/index';
+export { default as Alert } from './components/Alert';
+export { default as Footer } from './components/Footer';
+export { default as Modal } from './components/Modal';
+export { default as Popup } from './components/Popup';
+export { default as Preloader } from './components/Preloader';
