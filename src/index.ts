a068fd2c56af290805ed42c80e1d0ddf4cd2f2ac
jejolare <jejolare@gmail.com>
2024-11-27 23:34:58 +0700

    add index.js exports

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+export * from './components/Alert';
+export * from './components/Footer';
+export * from './components/Modal';
+export * from './components/Popup';
+export * from './components/Preloader';
+export * from './utils/index';
