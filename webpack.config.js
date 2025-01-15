353326c0cd5696662313dc71c91768aa2943598e
jejolare <jejolare@gmail.com>
2025-01-16 00:21:49 +0700

    debug

--- webpack.config.js
+++ webpack.config.js
@@ -67,9 +67,9 @@ export default {
         ],
     },
     externals: {
-        'react': 'react',
-        'react-dom': 'react-dom',
-        'next': 'next',
+        react: 'React', 
+        'react-dom': 'ReactDOM',
+        next: 'next', 
     },
     experiments: {
         outputModule: true,
